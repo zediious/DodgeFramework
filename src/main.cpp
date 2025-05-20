39c8a5e81874349cdf4126c775b176c23f96615b
Jack <zediious@aol.com>
2025-05-20 15:41:08 -0400

    Move log init to own function
    Looking at DynamicCollisionAdjustment for this

--- src/main.cpp
+++ src/main.cpp
@@ -13,35 +13,40 @@ void MessageHandler(SKSE::MessagingInterface::Message* a_msg)
 	}
 }
 
-extern "C" DLLEXPORT bool SKSEAPI SKSEPlugin_Query(const SKSE::QueryInterface* a_skse, SKSE::PluginInfo* a_info)
+namespace
+{
+	void InitializeLog()
 {
 #ifndef NDEBUG
 	auto sink = std::make_shared<spdlog::sinks::msvc_sink_mt>();
 #else
 	auto path = logger::log_directory();
 	if (!path) {
-		return false;
+			SKSE::stl::report_and_fail("Failed to find standard logging directory"sv);
 	}
 
-	*path /= Plugin::NAME.data();
-	*path += ".log"sv;
+		*path /= fmt::format("{}.log"sv, Plugin::NAME);
 	auto sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>(path->string(), true);
 #endif
 
-	auto log = std::make_shared<spdlog::logger>("global log"s, std::move(sink));
-
 #ifndef NDEBUG
-	log->set_level(spdlog::level::trace);
+		const auto level = spdlog::level::trace;
 #else
-	log->set_level(spdlog::level::info);
-	log->flush_on(spdlog::level::info);
+		const auto level = spdlog::level::info;
 #endif
+
+		auto log = std::make_shared<spdlog::logger>("global log"s, std::move(sink));
+		log->set_level(level);
+		log->flush_on(level);
 
 	spdlog::set_default_logger(std::move(log));
 	spdlog::set_pattern("%g(%#): [%^%l%$] %v"s);
+	}
+}
 
-	// logger::info(FMT_STRING("{} v{}"), Plugin::PROJECT, Version::NAME);
 
+extern "C" DLLEXPORT bool SKSEAPI SKSEPlugin_Query(const SKSE::QueryInterface* a_skse, SKSE::PluginInfo* a_info)
+{
 	a_info->infoVersion = SKSE::PluginInfo::kVersion;
 	a_info->name = Plugin::NAME.data();
 	a_info->version = Plugin::VERSION[0];
@@ -62,6 +67,7 @@ extern "C" DLLEXPORT bool SKSEAPI SKSEPlugin_Query(const SKSE::QueryInterface* a
 
 extern "C" DLLEXPORT bool SKSEAPI SKSEPlugin_Load(const SKSE::LoadInterface* a_skse)
 {
+	InitializeLog();
 	logger::info("Dodge Framework loaded");
 
 	SKSE::Init(a_skse);
