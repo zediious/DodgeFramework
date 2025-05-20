dfe4d4d5427119f961055026dba1ba496d227005
Jack <zediious@aol.com>
2025-05-20 15:41:58 -0400

    Implement SKSEPlugin_Version
    This being missing was preventing the plugin from loading on versions post 1.5.97

--- src/main.cpp
+++ src/main.cpp
@@ -16,17 +16,17 @@ void MessageHandler(SKSE::MessagingInterface::Message* a_msg)
 namespace
 {
 	void InitializeLog()
-{
+	{
 #ifndef NDEBUG
-	auto sink = std::make_shared<spdlog::sinks::msvc_sink_mt>();
+		auto sink = std::make_shared<spdlog::sinks::msvc_sink_mt>();
 #else
-	auto path = logger::log_directory();
-	if (!path) {
+		auto path = logger::log_directory();
+		if (!path) {
 			SKSE::stl::report_and_fail("Failed to find standard logging directory"sv);
-	}
+		}
 
 		*path /= fmt::format("{}.log"sv, Plugin::NAME);
-	auto sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>(path->string(), true);
+		auto sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>(path->string(), true);
 #endif
 
 #ifndef NDEBUG
@@ -39,8 +39,8 @@ namespace
 		log->set_level(level);
 		log->flush_on(level);
 
-	spdlog::set_default_logger(std::move(log));
-	spdlog::set_pattern("%g(%#): [%^%l%$] %v"s);
+		spdlog::set_default_logger(std::move(log));
+		spdlog::set_pattern("%g(%#): [%^%l%$] %v"s);
 	}
 }
 
@@ -65,8 +65,22 @@ extern "C" DLLEXPORT bool SKSEAPI SKSEPlugin_Query(const SKSE::QueryInterface* a
 	return true;
 }
 
+// Function for AE loading
+extern "C" DLLEXPORT constinit auto SKSEPlugin_Version = []() {
+	SKSE::PluginVersionData v;
+
+	v.PluginVersion(Plugin::VERSION);
+	v.PluginName(Plugin::NAME);
+	v.AuthorName("Ersh");
+	v.UsesAddressLibrary(true);
+	v.CompatibleVersions({ SKSE::RUNTIME_SSE_LATEST });
+	v.HasNoStructUse(true);
+
+	return v;
+}();
+
 extern "C" DLLEXPORT bool SKSEAPI SKSEPlugin_Load(const SKSE::LoadInterface* a_skse)
-{
+{	
 	InitializeLog();
 	logger::info("Dodge Framework loaded");
 
